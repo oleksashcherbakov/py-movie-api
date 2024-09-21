f805255fbf6598d1727198407813566e331d3594
oleksashcherbakov <oleksa.shcherbakov@gmail.com>
2024-09-21 17:36:20 +0300

    'Solution'

---
+++ cinema/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Movie(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    duration = models.IntegerField()
+
+    def __str__(self):
+        return self.title
